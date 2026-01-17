7d117be4b997a412c5f1c837e40e62add5d61abd
Ratika1004 <aroraratika10@gmail.com>
2026-01-17 13:33:28 -0500

    added user module with profile apis

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 const connectDB= require("./shared/utils/connect-db");
 const authRoutes = require("./modules/auth/auth.routes");
+const userRoutes =  require("./modules/user/user.routes");
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(connectDB);
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT , () => {
